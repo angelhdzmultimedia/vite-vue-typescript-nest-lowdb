ed431912c64328ded7a100eb61fa4b218d69b665
angelhdzmultimedia <angel.hernandez.12@live.com>
2022-07-10 19:13:30 -0400

    2 files modified

--- server/nest-app/src/app.controller.ts
+++ server/nest-app/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ShutdownService } from './shutdown/shutdown.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly shutdownService: ShutdownService
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('shutdown')
+  shutdown() {
+    this.shutdownService.emit('shutdown')
   }
 }
--- server/nest-app/src/shutdown/shutdown.service.ts
+++ server/nest-app/src/shutdown/shutdown.service.ts
@@ -3,8 +3,4 @@ import { Injectable } from '@nestjs/common';
 import { EventEmitter } from 'events';
 
 @Injectable()
-export class ShutdownService extends EventEmitter {
-  public shutdown() {
-    this.emit('shutdown');
-  }
-}
+export class ShutdownService extends EventEmitter {}
