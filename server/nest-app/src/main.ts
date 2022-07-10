aa801fd244eaaababbe9fddf024e37252af37502
angelhdzmultimedia <angel.hernandez.12@live.com>
2022-07-10 04:12:35 -0400

    3 files modified

--- server/nest-app/src/main.ts
+++ server/nest-app/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import kill from 'kill-port';
 async function bootstrap() {
+  await kill(5000, 'tcp');
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(5000);
