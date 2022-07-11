150e5a12fe1197879445e6d88cb3df7a3c8dda8d
angelhdzmultimedia <angel.hernandez.12@live.com>
2022-07-11 08:16:34 -0400

    1 file modified

--- src/pages/private/index.vue
+++ src/pages/private/index.vue
@@ -1,22 +1,34 @@
 <template>
   <MainLayout>
     <template #page>
-      <q-page class="column  items-center full-width">
+      <q-page class="column items-center full-width">
         <q-card class="column q-mt-md items-center q-pa-md q-gutter-md">
-         <q-card-section>
-          <span class="text-h6 text-bold">Create Post</span>
-         
-         </q-card-section>
-          <q-separator dark/>
-           <q-editor
+          <q-card-section>
+            <span class="text-h6 text-bold">Create Post</span>
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+                </q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>Title</q-item-label>
+                <q-item-label caption>Subhead</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-card-section>
+          <q-separator dark />
+
+          <q-editor
             v-model="formData.post.newPost"
             :placeholder="whatAreYouThinking"
           >
           </q-editor>
-           <q-card-actions>
-         
-          <q-btn>POST</q-btn>
-           </q-card-actions>
+
+          <q-card-actions class="full-width">
+            <q-btn class="full-width">POST</q-btn>
+          </q-card-actions>
         </q-card>
       </q-page>
     </template>
