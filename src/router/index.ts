923991061e69596cdbe27bf7b00ae3736bacdef1
angelhdzmultimedia <angel.hernandez.12@live.com>
2022-07-11 07:37:28 -0400

    1 file modified

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { useAuthStore } from '../stores/auth';
 
+function generateRoutes(pages, access) {
+  let routes = [];
+  for (const page in pages) {
+    const token = /^(.*[\\/])?(.*?)(\.[^.]*?|)$/gi.exec(page);
+    const data = {
+      path: parts[0] || '',
+      subpath: parts[1] || '',
+      name: parts[2] || '',
+      extension: parts[3] || '',
+    };
+
+    let pathName = '';
+
+    if (data.name === 'index') {
+      pathName = '/';
+    } else {
+      pathName = `/${data.name.toLowerCase().replace('page', '')}`;
+    }
+
+    routes.push({
+      path: pathName,
+      component: import(/* @vite-ignore */ pages[page]),
+      meta: {
+        auth: access === 'private',
+      },
+    });
+  }
+
+  return routes;
+}
+
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      component: async () => await import('../pages/DashboardPage.vue'),
-      meta: {
-        auth: true,
-      },
-    },
-    {
-      path: '/login',
-      component: async () => await import('../pages/LoginPage.vue'),
-      meta: {
-        auth: false,
-      },
-    },
-    {
-      path: '/register',
-      component: async () => await import('../pages/RegisterPage.vue'),
-      meta: {
-        auth: false,
-      },
-    },
-  ],
+  routes: generateRoutes(
+    import.meta.glob('../pages/public/*.vue'),
+    'public'
+  ).concat(
+    generateRoutes(import.meta.glob('../pages/private/*.vue'), 'private')
+  ),
 });
 
 router.beforeEach(async (to) => {
